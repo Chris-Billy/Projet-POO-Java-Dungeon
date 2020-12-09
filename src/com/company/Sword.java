76de551baed953f107a5277903bf71f11756b9ea
walid <walidhaddoury@outlook.fr>
2020-12-09 15:11:15 +0100

    ajout des constructor pour les class Equipement, Water_Flask et Sword

--- src/com/company/Sword.java
+++ src/com/company/Sword.java
@@ -1,4 +1,10 @@
 package com.company;
 
-public class Sword {
+public class Sword extends Equipments {
+    int swordDamage;
+
+    public Sword(String[] weaponType, int swordDamage) {
+        super(weaponType);
+        this.swordDamage = swordDamage;
+    }
 }
--- src/com/company/Water_flask.java
+++ src/com/company/Water_flask.java
@@ -1,4 +1,12 @@
 package com.company;
 
-public class Water_flask {
+public class Water_flask extends Equipments{
+    int stackWaterFlask;
+    int waterFlaskDamage;
+
+    public Water_flask(String[] weaponType, int stackWaterFlask, int waterFlaskDamage) {
+        super(weaponType);
+        this.stackWaterFlask = stackWaterFlask;
+        this.waterFlaskDamage = waterFlaskDamage;
+    }
 }
